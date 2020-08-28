c52c382e2bc151c04c91df7febd4d5925d676e44
athittaya <athittaya.ch@gmail.com>
2020-08-28 10:44:08 +0700

    Initial

---
+++ src/main/java/th/go/rd/bankaccount/controller/BankAccountRestController.java
@@ -0,0 +1,58 @@
+package th.go.rd.bankaccount.controller;
+
+import org.springframework.web.bind.annotation.*;
+import th.go.rd.bankaccount.data.BankAccountRepository;
+import th.go.rd.bankaccount.model.BankAccount;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/bankaccount")
+public class BankAccountRestController {
+    private BankAccountRepository repository;
+
+    public BankAccountRestController(BankAccountRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<BankAccount> getAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public BankAccount getOne(@PathVariable int id) {
+        return repository.findById(id).get();
+    }
+
+    @GetMapping("/customer/{id}")
+    public List<BankAccount> getAllCustomerId(@PathVariable int id) {
+        return repository.findByCustomerId(id);
+    }
+
+    @PostMapping
+    public BankAccount create(@RequestBody BankAccount bankAccount){
+        repository.save(bankAccount);
+        return repository.findById(bankAccount.getId()).get();
+    }
+
+    @PutMapping("/{id}")
+    public BankAccount update(@PathVariable int id,
+                              @RequestBody BankAccount bankAccount) {
+        BankAccount record = repository.findById(id).get();
+        record.setBalance(bankAccount.getBalance());
+        repository.save(record);
+        return record;
+    }
+
+
+    @DeleteMapping("/{id}")
+    public BankAccount delete(@PathVariable int id) {
+        BankAccount record = repository.findById(id).get();
+        repository.deleteById(id);
+        return record;
+    }
+
+
+
+}
